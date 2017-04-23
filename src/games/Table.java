d8d5135ff6b3ced72994029d3b7d566fe3021fec
Ryan Grillo <ryangrillo@gmail.com>
2017-04-23 15:54:38 -0600

    game logic works

--- src/games/Table.java
+++ src/games/Table.java
@@ -1,8 +1,10 @@
 package games;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Table {
-	
+
 	public void start() {
 		Scanner kb = new Scanner(System.in);
 		Player one = new Player("PlayerOne");
@@ -12,104 +14,97 @@ public class Table {
 		d.createDeck();
 		d.ShuffleCards();
 		Card c = d.getCard();
-		//System.out.println("C " + c);
-		//System.out.println(c.getCardNumber().getValue());
-		one.addCard(d.getCard());
-		one.addCard(d.getCard());
-		comp.addCard(d.getCard());
-		comp.addCard(d.getCard());
-		int compTotal = 0;
-		int oneTotal = 0;
-		
-		for (Card card : comp.getPlayerHand()) {
-			System.out.println("Computer: " + card);
-			compTotal = compTotal + card.getCardNumber().getValue();
-		}
-		System.out.println("Comp Total: " + compTotal);
-		for (Card card : one.getPlayerHand()) {
-			System.out.println("Player: " + card);
-			oneTotal = oneTotal + card.getCardNumber().getValue();
-		}
-		System.out.println("Player Total: " + oneTotal);
-		
-		while (oneTotal < 21) {
-			System.out.print(one.Name + "Would you like to (h)it or(s)tay: ");
-			oneTotal = 0;
-			String hitOrStay = kb.nextLine();
-			if (hitOrStay.equals("h")) {
-				one.addCard(d.getCard());
-				for (Card card : one.getPlayerHand()) {
-					System.out.println("Player: " + card);
-					oneTotal = oneTotal + card.getCardNumber().getValue();
-				}
-				System.out.println(one.getPlayerHand());
-				System.out.println(one.Name + " you have " + oneTotal + "points");
-				if(oneTotal>21){
-					System.out.println("You lost!");
-				}
+		// System.out.println("C " + c);
+		// System.out.println(c.getCardNumber().getValue());
+
+		boolean keepPlaying = true;
+		while (keepPlaying) {
+			one.addCard(d.getCard());
+			one.addCard(d.getCard());
+			comp.addCard(d.getCard());
+			comp.addCard(d.getCard());
+			int compTotal = 0;
+			int oneTotal = 0;
+
+			for (Card card : comp.getPlayerHand()) {
+				System.out.println("Computer: " + card);
+				compTotal = compTotal + card.getCardNumber().getValue();
 			}
-			else{
-				compTotal = 0;
-				while(compTotal<17){
-					compTotal = 0;
-					comp.addCard(d.getCard());
+			System.out.println("Comp Total: " + compTotal);
+			for (Card card : one.getPlayerHand()) {
+				System.out.println("Player: " + card);
+				oneTotal = oneTotal + card.getCardNumber().getValue();
+			}
+			System.out.println("Player Total: " + oneTotal);
+			boolean keepPlayingHand = true;
+			while (keepPlayingHand) {
+				System.out.print(one.Name + "Would you like to (h)it or(s)tay: ");
+				oneTotal = 0;
+				String hitOrStay = kb.nextLine();
+				if (hitOrStay.equals("h")) {
+					one.addCard(d.getCard());
 					for (Card card : one.getPlayerHand()) {
 						System.out.println("Player: " + card);
-						compTotal = compTotal + card.getCardNumber().getValue();
+						oneTotal = oneTotal + card.getCardNumber().getValue();
+					}
+					System.out.println(one.getPlayerHand());
+					System.out.println(one.Name + " you have " + oneTotal + "points");
+					if (oneTotal > 21) {
+						System.out.println("You lost!");
+						System.out.println("You bust");
+						keepPlayingHand = false;
+						oneTotal = 0;
+						compTotal = 0;
+						one.setPlayerHand(new ArrayList<Card>());
+						comp.setPlayerHand(new ArrayList<Card>());
+					}
+				} else {
+					compTotal = 0;
+					while (compTotal < 17) {
+						compTotal = 0;
+						comp.addCard(d.getCard());
+						for (Card card : comp.getPlayerHand()) {
+							System.out.println("Comp: " + card);
+							compTotal = compTotal + card.getCardNumber().getValue();
+						}
+					}
+					for (Card card : one.getPlayerHand()) {
+						oneTotal = oneTotal + card.getCardNumber().getValue();
+					}
+					System.out.println(comp.getPlayerHand());
+					System.out.println(comp.Name + " you have " + compTotal + " points");
+					// call checkForWin method pass in oneTotal and compTotal
+					if (compTotal > 21) {
+						System.out.println("You win!");
+						keepPlayingHand = false;
+					} else {
+						checkForWin(oneTotal, compTotal);
+						keepPlayingHand = false;
 					}
 				}
-				System.out.println(comp.getPlayerHand());
-				System.out.println(comp.Name + " you have " + compTotal + "points");
-				//call checkForWin method pass in oneTotal and compTotal
-				break;
-			}
-		}
-		
-		
-		
-		
-		
-//		Deck deck = new Deck();
-//		deck.ShuffleCards();
-//	 
-//		System.out.println("Player 1");
-		//System.out.println(deck.getCard(deck));
-//		deck.getDeck().get(0).getCardNumber().getValue(); //takes card value and store it as int
-//		deck.getCard();
-//		deck.getDeck().get(0).getCardNumber().getValue(); //takes card value and store it as int
-		
-		
-//		int playerCardValueCardOne = deck.getDeck().get(0).getCardNumber().getValue(); //takes card value and store it as int
-//	    System.out.println(deck.getDeck().remove(0));//removes card from deck
-//		int playerCardValueCardTwo = deck.getDeck().get(0).getCardNumber().getValue(); //takes card value and store it as int
-//		System.out.print(deck.getDeck().remove(0) + "\n\n");
-//		System.out.println("playercardvalue: " + playerCardValueCardOne);
-		
-//		System.out.println("Computer:");
-//		Integer computerCardValueCardOne = deck.getDeck().get(0).getCardNumber().getValue(); //takes card value and store it as int
-//	    System.out.print(deck.getDeck().remove(0) + "\n");//removes card from deck
-//		Integer computerCardValueCardTwo = deck.getDeck().get(0).getCardNumber().getValue(); //takes card value and store it 
-//		System.out.println(deck.getDeck().remove(0) + "\n");
 
-//		int playerTotal = playerCardValueCardOne + playerCardValueCardTwo;
-//		int compTotal = computerCardValueCardOne + computerCardValueCardTwo;
-		
-//		System.out.println("Player  1 you have a total of " + playerTotal + "points");
-//		System.out.println("Computer has a total of " + compTotal + "points");
-//		
-//		
-//		
-//		while (playerTotal <= 56 && compTotal <= 56) {
-//			System.out.println("would you like to (h)it or (s)stay");
-//			String hitOrStay = kb.next();
-//			if (hitOrStay.equals("h")) {
-//				System.out.println(deck.getDeck().remove(0));
-//				System.out.println("new total: " + (playerTotal + deck.getDeck().get(0).getCardNumber().getValue()));
-//			}
-//		}
-		
-		
-		//System.out.println(deck.getDeck().size()+ "is the size now of the deck");
-		
+			}
+			oneTotal = 0;
+			compTotal = 0;
+			one.setPlayerHand(new ArrayList<Card>());
+			comp.setPlayerHand(new ArrayList<Card>());
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
 		}
 	}
+
+	public void checkForWin(int playerTotal, int compTotal) {
+		System.out.println(playerTotal);
+		System.out.println(compTotal);
+		if (playerTotal > compTotal) {
+			System.out.println("Player 1 wins this hand");
+		} else if (playerTotal == compTotal) {
+			System.out.println("You push");
+		} else {
+			System.out.println("computer wins");
+		}
+	}
+}
